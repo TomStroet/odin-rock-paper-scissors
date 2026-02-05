b5a734dfd8fee770a882019e4e9c0a754b0c0b1a
Tom <194397336+TomStroet@users.noreply.github.com>
2026-02-05 15:00:41 +0100

    add playRound fn

--- script.js
+++ script.js
@@ -21,10 +21,6 @@ Steps
 
 */
 
-
-// 1. write function that returns computer choice: random pick of r/p/s.
-// WHEN called, the function returns a random pick of r/p/s. The function has no input.
-
 function getComputerChoice() {
     const randomPick = Math.random() * 3;
     if (randomPick < 1) {
@@ -34,11 +30,49 @@ function getComputerChoice() {
     } else return 'scissors'
 }
 
-console.log(getComputerChoice())
-
 function getHumanChoice() {
     const choice = prompt('Enter your choice')
-    return choice
+    return choice.toLowerCase()
 }
 
-console.log(getHumanChoice())
+function playRound(humanChoice, computerChoice) {
+    console.log(`You pick ${humanChoice}`)
+    console.log(`Computer picks ${computerChoice}`)
+
+    let result
+    if (humanChoice == computerChoice) {
+        result = 0
+    } else if (humanChoice == 'rock' && computerChoice == 'scissors' 
+        || humanChoice == 'paper' && computerChoice == 'rock'
+        || humanChoice == 'scissors' && computerChoice == 'paper') {
+        result = 1
+    } else {
+        result = -1
+    }
+
+    logResult(result)
+    updateScore(result)
+}
+
+function updateScore(result) {
+    if (result == 1) {
+        humanScore++;
+    } else if (result == -1) {
+        computerScore++;
+    }
+    console.log(`Score - You: ${humanScore} - Computer: ${computerScore}`);
+}
+
+function logResult(result) {
+    if (result == 1) {
+        console.log(`You win! ${humanChoice} beats ${computerChoice}.`)
+    } else if (result == -1) {
+        console.log(`You lose! ${computerChoice} beats ${humanChoice}.`)
+    } else console.log('Draw!')
+}
+
+let humanScore = 0, computerScore = 0
+
+const humanChoice = getHumanChoice()
+const computerChoice = getComputerChoice()
+playRound(humanChoice, computerChoice)
